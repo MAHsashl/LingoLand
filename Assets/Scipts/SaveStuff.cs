272de21348882107e79ea2ad02d50e7d9cacd251
MAHsaashl <38511456+MAHsaashl@users.noreply.github.com>
2018-04-20 00:18:57 +0430

    Initial commit

---
+++ Assets/Scipts/SaveStuff.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Backtory.Core.Public;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.IO;
+using Backtory.InAppPurchase.Public;
+
+public class SaveStuff : MonoBehaviour {
+
+
+    public InputField usernameText;
+    public InputField passwordText;
+    string userText;
+    string passText;
+
+    void Start(){
+
+        userText = PlayerPrefs.GetString("userTextKeyName");
+        passText = PlayerPrefs.GetString("passTextKeyName");
+        usernameText.text = userText;
+        passwordText.text = passText;
+         
+    }
+
+    public void SaveThis(){
+
+        userText = usernameText.text;
+        passText = passwordText.text;
+
+        PlayerPrefs.SetString("userTextKeyName", userText);
+        PlayerPrefs.SetString("passTextKeyName", passText);
+
+    }
+
+    public void onLoginClick()
+    {
+        // Pass user info to login 
+        BacktoryUser.LoginInBackground(userText, passText, loginResponse => {
+
+            // Login operation done (fail or success), handling it:
+            if (loginResponse.Successful)
+            {
+                // Login successful
+                Debug.Log("Welcome " + userText);
+            }
+            else if (loginResponse.Code == (int)BacktoryHttpStatusCode.Unauthorized)
+            {
+                // Username 'mohx' with password '123456' is wrong
+                Debug.Log("Either username or password is wrong.");
+            }
+            else
+            {
+                // Operation generally failed, maybe internet connection issue
+                Debug.Log("Login failed for other reasons like network issues.");
+            }
+        });
+    }
+
+
+
+}
---
+++ Assets/Scipts/Signup_Panel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Backtory.Core.Public;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.IO;
+using Backtory.InAppPurchase.Public;
+
+public class Signup_Panel : MonoBehaviour {
+
+    // Signup Panel
+    public InputField usernameInput;
+    public InputField emailInput;
+    public InputField passwordInput;
+    int m_DropdownValueage;
+    int m_DropdownValuegen;
+    string userID;
+
+    public void GetAge(int valage)
+    {
+        m_DropdownValueage = valage;
+        Debug.Log(m_DropdownValueage);
+    }
+
+    public void GetGen(int valgen)
+    {
+        m_DropdownValuegen = valgen;
+        Debug.Log(m_DropdownValuegen);
+    }
+    public void onRegisterClick()
+    {
+
+        BacktoryUser currentUser = BacktoryUser.CurrentUser;
+        userID = currentUser.UserId;
+
+
+        // First create a user and fill his/her data
+        BacktoryUser newUser = new BacktoryUser
+        {
+
+            Username = usernameInput.text,
+            Email = emailInput.text,
+            Password = passwordInput.text,
+
+        };
+        // Registring user to backtory (in background)
+        newUser.RegisterInBackground(response =>
+        {
+            // Checking result of operation
+            if (response.Successful)
+            {
+                Debug.Log("Register Success; new username is " + response.Body.Username);
+                // Get current user of system
+            }
+            else if (response.Code == (int)BacktoryHttpStatusCode.Conflict)
+            {
+                // Username is invalid
+                Debug.Log("Bad username; a user with this username already exists.");
+            }
+            else
+            {
+                // General failure
+                Debug.Log("Registration failed; for network or some other reasons.");
+            }
+        });
+    }
+
+    public void saveAgegen()
+    {
+
+        BacktoryObject genderage = new BacktoryObject("GenderAge");
+        genderage["gender"] = m_DropdownValueage;
+        genderage["age"] = m_DropdownValuegen;
+        genderage["userID"] = userID;
+
+       
+        genderage.SaveInBackground(response =>
+        {
+            if (response.Successful)
+            {
+                // successful save. good place for Debug.Log function.
+
+            }
+            else
+            {
+                // see response.Message to know the cause of error
+            }
+        });
+    }
+}
